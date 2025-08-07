efd0ee3705a8b4a1c762c1ee4d1b1768d183ae3a
Jacob Tkaczyk <tkaczykj@umich.edu>
2025-08-07 00:52:40 -0400

    Update layout.tsx
    removed authgaurd

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AuthGuard from "@/components/AuthGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
