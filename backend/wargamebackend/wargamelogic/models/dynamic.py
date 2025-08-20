6c741fe885a248270d26cf123a28e8c12eda6e27
Jacob <tkaczykj@umich.edu>
2025-08-20 18:00:27 -0400

    game instance fix

--- backend/wargamebackend/wargamelogic/models/dynamic.py
+++ backend/wargamebackend/wargamelogic/models/dynamic.py
@@ -5,6 +5,7 @@ from .static import Role, Team, Unit, Tile, Landmark
 class GameInstance(models.Model):
     join_code = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_started = models.BooleanField(default=False) 
 
     def __str__(self):
         return f"GameInstance: {self.join_code}"
